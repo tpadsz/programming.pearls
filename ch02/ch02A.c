c624ce6841277ed6d2b48e9c8c2742fafaf0bb58
王鑫康 <roger.wang@tpadsz.com>
2016-02-05 11:16:39 +0800

    Added 4 billion numbers generation.

--- ch02/ch02A.c
+++ ch02/ch02A.c
@@ -10,16 +10,21 @@
 /**
 *   Prepares the file which contains <NUMBERS> numbers.
 *   If the file already exists, will clear it.
+*   Buckle up! In this problem, NUMBERS is 4 billion and
+*   this function produces a text file with 42GB in size!
+*   Check your disk space first!
 */
 FILE* prepare(){
-    FILE* fp=fopen("d:\\numbers.txt","w+");
+    FILE* fp=fopen("numbers.txt","w+");
     if(fp==NULL){
         perror("Failed to open file.");
         exit(1);
     }
     unsigned int i;
     for(i=0;i<NUMBERS;i++){
-        fprintf(fp,"%d\n",i);
+        // Using unsigned numbers for programmers
+        // to check them clearly.
+        fprintf(fp,"%u\n",i);
     }
     return fp;
 }
--- ch02/ch02A.h
+++ ch02/ch02A.h
@@ -1,6 +1,9 @@
 #ifndef CH02A_H_INCLUDED
 #define CH02A_H_INCLUDED
 
+// As defined in the problem, we will search 4 billion
+// numbers for at least one missing. 0xEE6B2800 literally
+// is the hex representation for 4 billion.
 #define NUMBERS 0xEE6B2800
 
 #endif // CH02A_H_INCLUDED
